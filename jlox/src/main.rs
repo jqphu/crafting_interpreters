f4e4af2fd44aba150d5d686b51c8859df6850c71
Justin Phu <me@justinphu.dev>
2021-12-19 13:39:41 +1100

    [jlox] Add basic reading arguments and logging
    This introduces the commands to both read from a file with --f as well
    as running in interactive.

--- jlox/src/main.rs
+++ jlox/src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "jlox")]
+struct Opt {
+    /// Input file
+    #[structopt(short, long, parse(from_os_str))]
+    file: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let opt = Opt::from_args();
+
+    if let Some(file) = opt.file {
+        log::info!("Reading input form file {:#?}.", &file);
+    } else {
+        log::info!("Running in interactive mode.");
+    }
 }
